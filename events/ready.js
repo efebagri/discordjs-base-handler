ea51e554a03991986539213172c5973a7d07c8bf
Efe Bağrı <81169584+Razetro@users.noreply.github.com>
2023-03-15 15:28:14 +0100

    Status updater added

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
 /*
  * User: efeba
- * Date/Time: 3/15/23, 1:10 PM
+s * Date/Time: 3/15/23, 3:20 PM
  * File: ready.js
  *
- * Modified: 3/15/23, 1:03 PM
+ * Modified: 3/15/23, 3:15 PM
  *
  * Copyright (c) 2023 BindYourServer (https://bindyourserver.com)
  *    All rights Reserved.
@@ -11,7 +11,19 @@
 
 const client = require("../index");
 
+    let statuses = [
+        `BindYourServer`,
+        `x86-64`,
+        `Coding`,
+        `System`
+    ]
+
+    setInterval(function(){
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        client.user.setActivity(status, {type: 'STREAMING', url: "https://www.twitch.tv/razetro"});
+    }, 5000)
+
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}! v` + process.env.APP_VERSION);
+    console.log(`Bot => ${client.user.username} startet! v` + process.env.APP_VERSION);
     client.user.setActivity(process.env.APP_NAME + ` v` + process.env.APP_VERSION, { type: "STREAMING" });
 })

ad0c07f230efe4baf239dd1e67d3bc542d418d29
Efe Bağrı <81169584+efebagri@users.noreply.github.com>
2023-03-15 16:55:40 +0100

    Delete install.bat

--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-
-title ProblemAdmin Discord Bot by EfeBagri
-
-echo (%time%) NodeJS packets installing...
-npm i
-echo "Successfully Installed now open start.bat or run by node index"
-PAUSE

d298182cdf2e6b0203f68d6d5a7479576df42732
EfeBagri <efebagri@gmail.com>
2025-03-27 21:15:47 +0100

    chore: Remove unnecessary metadata comments from start.sh
    The metadata comments were redundant and contained outdated information. Cleaning them up improves readability and ensures the script remains focused on its functional purpose.

--- start.sh
+++ start.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#
-# User: efebagri
-# Date/Time: 2/12/24, 2:57 AM
-# File: start.sh
-#
-# Modified: 1/9/24, 2:40 PM
-#
-# Copyright (c) 2023-2024 Exbil (https://www.exbil.net/)
-#    All rights Reserved.
-#
-
 if [ -d "./node_modules/" ]; then
   npm start
 fi
